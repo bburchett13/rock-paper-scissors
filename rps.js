7524caec08be860934c4cc59556c2dc3d12eada8
Blake <bburchett13@gmail.com>
2024-01-14 20:36:56 -0500

    Added barebones css

--- rps.js
+++ rps.js
@@ -1,27 +1,35 @@
-const rps = ['Water', 'Grass', 'Fire']
+const rps = ['Water', 'Grass', 'Fire'];
+const wgf = ['💧', '🌱', '🔥'];
 
 // Function to play the game
 function rockPaperScissors(userChoice) {
-    let compChoice = rps[Math.floor(Math.random()*3)];
+    let random = Math.floor(Math.random()*3);
+    let compChoice = rps[random];
+    console.log(random);
+    userSign.textContent = wgf[rps.indexOf(userChoice)];
+    compSign.textContent = wgf[random];
 
     if (userChoice == compChoice){
 
-        gameRes.textContent = 'The Computer Chose ' + compChoice + '! It\'s a Tie!';
+        gameRes.textContent = 'It\'s a Tie!';
+        gameComment.textContent = userChoice + ' and ' + compChoice + ' tie!'
 
     }
     else if ((userChoice == 'Water' && compChoice == 'Grass') || (userChoice == 'Grass' && compChoice == 'Fire') || (userChoice == 'Fire' && compChoice == 'Water')){
         
         compScore++;
-        compScoreDisp.textContent = 'Computer Score: ' + compScore;
-        gameRes.textContent = 'The Computer Chose ' + compChoice + '! You Lose!';
+        compScoreDisp.textContent = 'Computer: ' + compScore;
+        gameRes.textContent = 'You Lose!';
+        gameComment.textContent = compChoice + ' beats ' + userChoice + '!';
 
         
     }
     else if ((userChoice == 'Grass' && compChoice == 'Water') || (userChoice == 'Fire' && compChoice == 'Grass') || (userChoice == 'Water' && compChoice == 'Fire')){
         
         userScore++;
-        userScoreDisp.textContent = 'User Score: ' + userScore;
-        gameRes.textContent = 'The Computer Chose ' + compChoice + '! You Win!';
+        userScoreDisp.textContent = 'Player: ' + userScore;
+        gameRes.textContent = 'You Win!';
+        gameComment.textContent = userChoice + ' beats ' + compChoice + '!';
 
     }
 }
@@ -29,33 +37,19 @@ function rockPaperScissors(userChoice) {
 let userScore = 0;
 let compScore = 0;
 
-// function to take result of game and update score
-function addScore (result) {
-
-    if (result == 3) {
-
-        userScore++;
-        userScoreDisp.textContent = 'User Score: ' + userScore;
-        gameRes.textContent = 'The Computer Chose' + compChoice + '! You Win!'
-
-    }
-    else if (result == 2){
-
-        compScore++;
-        compScoreDisp.textContent = 'Computer Score: ' + compScore;
-
-    }
-}
 
 const rockBtn = document.querySelector("#Rock");
 const paperBtn = document.querySelector("#Paper");
 const scissorsBtn = document.querySelector("#Scissors");
-const userScoreDisp = document.querySelector('#userScore')
-const compScoreDisp = document.querySelector('#compScore')
-const gameRes = document.querySelector('#gameResult')
+const userScoreDisp = document.querySelector('#userScore');
+const compScoreDisp = document.querySelector('#compScore');
+const gameRes = document.querySelector('#gameResult');
+const gameComment = document.querySelector('#gameComment');
+const userSign = document.querySelector('.userSign');
+const compSign = document.querySelector('.compSign');
 
-userScoreDisp.textContent = 'User Score: ' + userScore;
-compScoreDisp.textContent = 'Computer Score: ' + compScore;
+userScoreDisp.textContent = 'Player: ' + userScore;
+compScoreDisp.textContent = 'Computer: ' + compScore;
 gameRes.textContent = 'Press a button to play!'
 
 let userChoice;
